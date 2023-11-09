d2cdf08d6aa21fbb6a64ca399af76ad9ea0287d2
Nastavnic <arya.naufal.andt@gmail.com>
2023-11-09 20:58:57 +0700

    Add tailwind config color and dashboard breakpoint

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,12 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+      colors: {
+        'orange': '#FF7E33',
+        
+      }
+    },
 	},
 	plugins: [],
 }
